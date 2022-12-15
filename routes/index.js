2e754d9d9a2acf6d1b7ae810e152328316893856
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2022-12-15 11:26:51 -0330

    Login template
    Adding a template for login options to configure. Also updated footer.ejs and nav.ejs to just look a little more clean.

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const loginController = require("../controllers/loginControllers");
+// const bcrypt = require("bcrypt");
+// const uuid = require("uuid");
+const router = express.Router();
+
+if (DEBUG) {
+  console.log("ROUTE: /api/autos");
+}
+
+router.use(express.static("public"));
+
+router.get("/", async (req, res) => {
+  if (DEBUG) console.log("login page: ");
+  res.render("login", { status: req.app.locals.status });
+});
+
+router.post("/", loginController.getByUsername);
+
+// from http browser it has /auth/new
+router.get("/new", async (req, res) => {
+  res.render("register", { status: req.app.locals.status });
+});
+
+router.post("/new", loginController.createUser);
+
+router.get("/exit", async (req, res) => {
+  if (DEBUG) console.log("get /exit");
+  res.redirect("/");
+});
+
+module.exports = router;
+
+// router.use('/autos', require('./autos'));
+
+module.exports = router;
