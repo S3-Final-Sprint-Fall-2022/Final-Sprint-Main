84901c2ffc5e6d1461be455df51bb30468f81afb
Col-Ian <100207902+Col-Ian@users.noreply.github.com>
2022-12-20 21:13:41 -0330

    MongoDB working

--- routes/cars.js
+++ routes/cars.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const carsDal = require("../services/p.car_data.dal");
+const Car = require("../services/m.db");
 
 // load the logEvents module
 const logEvents = require("../services/logEvents");
@@ -12,26 +13,44 @@ const myEmitter = new MyEmitter();
 // add the listener for the logEvent
 myEmitter.on("log", (event, level, msg) => logEvents(event, level, msg));
 
-router.get("/", async (req, res) => {
-  // const theCars = [
-  //     { car_make: "Ford", car_model: "Bronco", car_model_year: "1989" },
-  //     { car_make: "Suzuki", car_model: "Verona", car_model_year: "2005" },
-  //     { car_make: "Infiniti", car_model: "Q", car_model_year: "2003" },
-  //     { car_make: "Buick", car_model: "Enclave", car_model_year: "2009" },
-  // ];
-  try {
-    let theCars = await carsDal.getCarData(); // from postgresql
-    myEmitter.emit(
-      "log",
-      "/cars GET",
-      "INFO",
-      "successfully listed all the cars"
-    );
-    res.render("cars", { theCars });
-  } catch {
-    res.render("503");
-  }
-});
+const cars_index = (req, res, next) => {
+  Car.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      res.render("cars", {
+        cars: result,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  //   next();
+};
+
+router.get(
+  "/",
+  cars_index
+  // , async (req, res) => {
+  //   // const theCars = [
+  //   //     { car_make: "Ford", car_model: "Bronco", car_model_year: "1989" },
+  //   //     { car_make: "Suzuki", car_model: "Verona", car_model_year: "2005" },
+  //   //     { car_make: "Infiniti", car_model: "Q", car_model_year: "2003" },
+  //   //     { car_make: "Buick", car_model: "Enclave", car_model_year: "2009" },
+  //   // ];
+  //   try {
+  //     let theCars = await carsDal.getCarData(); // from postgresql
+  //     myEmitter.emit(
+  //       "log",
+  //       "/cars GET",
+  //       "INFO",
+  //       "successfully listed all the cars"
+  //     );
+  //     res.render("cars", { theCars });
+  //   } catch {
+  //     res.render("503");
+  //   }
+  // }
+);
 
 router.post("/search/", async (req, res) => {});
 
